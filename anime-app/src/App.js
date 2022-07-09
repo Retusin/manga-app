06b53c21f88c69fa75a60a6fd24952e01f9addb9
Denis <retusin83@gmail.com>
2022-07-09 18:24:25 +0300

    first commit

---
+++ anime-app/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center font-semibold text-3xl">
+      {' '}
+      Тут будет сайт про аниме 🧡
+    </div>
+  );
+}
+
+export default App;
