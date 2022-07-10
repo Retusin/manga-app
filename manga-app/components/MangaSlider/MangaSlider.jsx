fea3195a920b7a53d5a039ccb9ecf00d97541651
Denis <retusin83@gmail.com>
2022-07-10 23:28:29 +0300

    Feat: Create Slider

---
+++ manga-app/components/MangaSlider/MangaSlider.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Zoom} from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import Image from 'next/image';
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from 'react-icons/ai';
+
+import image1 from '../../assets/images/slider-1.jpg';
+import image2 from '../../assets/images/slider-2.jpg';
+import image3 from '../../assets/images/slider-3.jpg';
+import image4 from '../../assets/images/slider-4.jpg';
+
+const MangaSlider = () => {
+  const images = [image1, image2, image3, image4];
+
+  const zoomInProperties = {
+    indicators: true,
+    scale: 1.2,
+    duration: 5000,
+    transitionDuration: 500,
+    infinity: true,
+
+    prevArrow: (
+      <div style={{width: '20px', marginRight: '0px', cursor: 'pointer'}}>
+        <AiOutlineArrowLeft size={25} className="text-pink-500" />
+      </div>
+    ),
+
+    nextArrow: (
+      <div style={{width: '-20px', marginRight: '0px', cursor: 'pointer'}}>
+        <AiOutlineArrowRight size={25} className="text-pink-500" />
+      </div>
+    ),
+  };
+  return (
+    <div className="container">
+      <div className="my-10">
+        <h1 className="text-center text-3xl font-semibold mb-6">Posters</h1>
+        <Zoom {...zoomInProperties}>
+          {images.map((each, i) => (
+            <div key={i} className="flex justify-center w-full h-full">
+              <Image
+                src={each}
+                className="w-3/4 object-cover rounded-xl"
+                width="600px"
+                height="600px"
+                alt="slider"
+              />
+            </div>
+          ))}
+        </Zoom>
+      </div>
+    </div>
+  );
+};
+
+export default MangaSlider;
--- manga-app/pages/index.js
+++ manga-app/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import Hero from '../components/Hero/Hero';
+import MangaSlider from './../components/MangaSlider/MangaSlider';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
+      <MangaSlider />
     </div>
   );
 }
