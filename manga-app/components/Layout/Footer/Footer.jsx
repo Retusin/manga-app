891bff46f029d983c3218d9ad0b07d13532ded22
Denis <retusin83@gmail.com>
2022-07-13 20:04:51 +0300

    Feat: Optimi img

--- manga-app/components/Layout/Footer/Footer.jsx
+++ manga-app/components/Layout/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="p-2 text-center bg-[#141324] z-10">
+      <h2>Made with 🧡</h2>
+    </div>
+  );
 };
 
 export default Footer;
