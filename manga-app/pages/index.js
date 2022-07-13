291ad6ac35a52aceb93409399addfac6b6d37930
Denis <retusin83@gmail.com>
2022-07-13 22:45:49 +0300

    Feat: remove Video

--- manga-app/pages/index.js
+++ manga-app/pages/index.js
@@ -24,12 +24,10 @@ export default function Home({anime, manga}) {
 }
 
 export const getStaticProps = async () => {
-  const anime = await fetch('https://api.jikan.moe/v4/top/anime');
+  const anime = await fetch('https://kitsu.io/api/edge/trending/anime');
   const manga = await fetch('https://kitsu.io/api/edge/trending/manga');
   const animeData = await anime.json();
   const mangaData = await manga.json();
-
-  console.log(animeData);
 
   return {
     props: {
